c9215c4d8bbb010443074393e79bd9598aa48706
Mark Percival <mark@mpercival.com>
2009-10-26 08:18:31 -0400

    example fixed, was being configured twice

--- examples/create_hit.rb
+++ examples/create_hit.rb
@@ -5,10 +5,6 @@ require '../lib/rturk'
 aws = YAML.load(File.open(File.join(File.dirname(__FILE__), 'mturk.yml')))
 RTurk::setup(aws['AWSAccessKeyId'], aws['AWSAccessKey'], :sandbox => true)
 
-root = File.expand_path(File.dirname(__FILE__))
-aws = YAML.load(File.open(File.join(root, 'mturk.yml')))
-RTurk::setup(aws['AWSAccessKeyId'], aws['AWSAccessKey'], :sandbox => true)
-
 hit = RTurk::Hit.create(:title => 'Write a tweet for me') do |hit|
   hit.description = 'Simply write a witty twitter update for my account'
   hit.reward = 0.05
