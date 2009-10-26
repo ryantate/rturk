709137bca7c4ffa4f9962fe1f099d8d882dd6acb
Mark Percival <mark@mpercival.com>
2009-10-26 08:16:46 -0400

    update a test

--- lib/rturk/adapters/assignment.rb
+++ lib/rturk/adapters/assignment.rb
@@ -1,7 +1,7 @@
 require 'time'
 
 module RTurk
-  
+
   # Parses:
   # <Assignment>
   #   <AssignmentId>GYFTRHZ5J3DZREY48WNZE38ZR9RR1ZPMXGWE7WE0</AssignmentId>
@@ -22,28 +22,28 @@ module RTurk
   #     </QuestionFormAnswers>
   #   </Answer>
   # </Assignment>
-  
+
   class Assignment
     include RTurk::XmlUtilities
-    
+
     attr_accessor :worker_id, :id, :status, :submitted_at, :accepted_at, :approved_at
     attr_reader :answers
-    
+
     def initialize(xml_object)
       map_content(xml_object,
-        :id => 'AssignmentId',
-        :worker_id => 'WorkerId',
-        :status => 'AssignmentStatus',
-        :accepted_at => 'AcceptTime',
-        :approved_at => "ApprovalTime",
-        :submitted_at => 'SubmitTime')
-        self.answers = xml_object.xpath('Answer').children.to_s
+                  :id => 'AssignmentId',
+                  :worker_id => 'WorkerId',
+                  :status => 'AssignmentStatus',
+                  :accepted_at => 'AcceptTime',
+                  :approved_at => "ApprovalTime",
+                  :submitted_at => 'SubmitTime')
+      self.answers = xml_object.xpath('Answer')
     end
-    
+
     def approved_at=(time)
       @approved_at = Time.parse(time)
     end
-    
+
     def submitted_at=(time)
       @submitted_at = Time.parse(time)
     end
@@ -51,25 +51,23 @@ module RTurk
     def accepted_at=(time)
       @accepted_at = Time.parse(time)
     end
-    
+
     def approve!(feedback = nil)
       RTurk::ApproveAssignment(:assignment_id => self.id, :feedback => feedback)
     end
-    
+
     def reject!(reason)
       RTurk::ApproveAssignment(:assignment_id => self.id, :feedback => reason)
     end
-    
+
     def bonus!(amount, reason)
-     RTurk::GrantBonus(:amount, :feedback => reason)
+      RTurk::GrantBonus(:amount, :feedback => reason)
     end
-    
+
     def answers=(answer_xml)
-      unless answer_xml.empty?
-        @answers = RTurk::Answers.new(answer_xml)
-      end
+      @answers = RTurk::Answers.new(answer_xml.children.to_s)
     end
-    
+
   end
-  
+
 end
--- spec/operations/get_assignments_spec.rb
+++ spec/operations/get_assignments_spec.rb
@@ -1,26 +1,48 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
-describe "getting assignments" do
-
+describe RTurk::GetAssignmentsForHIT do
+  
   before(:all) do
     aws = YAML.load(File.open(File.join(SPEC_ROOT, 'mturk.yml')))
     RTurk.setup(aws['AWSAccessKeyId'], aws['AWSAccessKey'], :sandbox => true)
-    faker('get_assignments', :operation => 'GetAssignmentsForHIT')
   end
 
   it "should ensure required params" do
     lambda{RTurk::GetAssignmentsForHIT(:page_number => 5)}.should raise_error RTurk::MissingParameters
   end
-  
+
   it "should successfully request an assignment" do
     RTurk::Requester.should_receive(:request).once.with(
-      hash_including('Operation' => 'GetAssignmentsForHIT'))
+    hash_including('Operation' => 'GetAssignmentsForHIT'))
     RTurk::GetAssignmentsForHIT(:hit_id => "abcd") rescue RTurk::InvalidRequest
   end
+
+  context "an HIT with one assignment" do
+    
+    before(:all) do
+      FakeWeb.clean_registry
+      faker('get_assignments', :operation => 'GetAssignmentsForHIT')
+    end
+
+    it "should parse and return and answer" do
+      assignments = RTurk::GetAssignmentsForHIT(:hit_id => "abcd").assignments
+      assignments.first.answers['tweet'].should eql("This is my tweet!")
+    end
+
+  end
   
-  it "should parse and return and answer" do
-    assignments = RTurk::GetAssignmentsForHIT(:hit_id => "abcd").assignments
-    assignments.first.answers['tweet'].should eql("This is my tweet!")
+  context "an HIT with multiple assignment" do
+
+    before(:all) do
+      FakeWeb.clean_registry
+      faker('get_assignments_multiple', :operation => 'GetAssignmentsForHIT')
+    end
+
+    it "should parse and return and answer" do
+      assignments = RTurk::GetAssignmentsForHIT(:hit_id => "abcd").assignments
+      assignments.first.answers['tweet'].should eql("This is my tweet!")
+    end
+
   end
 
 end
