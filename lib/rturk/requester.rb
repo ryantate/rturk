b243805aa1b5f63f8cdf971fcae8bc433a55944e
Tyler <tyler@speakertext.com>
2010-06-23 12:35:19 -0400

    Make sign method public to enable checking notification signature

--- lib/rturk/requester.rb
+++ lib/rturk/requester.rb
@@ -33,16 +33,16 @@ module RTurk
         RTurk.logger.debug "Sending request:\n\t #{credentials.host}?#{querystring}"
         RestClient.get("#{credentials.host}?#{querystring}")
       end
-
+      
+      def sign(secret_key, service,method,time)
+        msg = "#{service}#{method}#{time}"
+        return hmac_sha1(secret_key, msg )
+      end
+      
       private
       
         def credentials
           RTurk
-        end
-
-        def sign(secret_key, service,method,time)
-          msg = "#{service}#{method}#{time}"
-          return hmac_sha1(secret_key, msg )
         end
 
         def hmac_sha1(key, s)
