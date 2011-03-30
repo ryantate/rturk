11fe8b5669582ce959fbc2b86d6359b37cf699e0
Haris Amin <aminharis7@gmail.com>
2011-03-30 12:34:49 -0400

    fixed typo in spec

--- spec/operations/reject_qualification_request_spec.rb
+++ spec/operations/reject_qualification_request_spec.rb
@@ -15,11 +15,11 @@ describe RTurk::RejectQualificationRequest do
   it "should successfully request the operation" do
     RTurk::Requester.should_receive(:request).once.with(
       hash_including('Operation' => 'RejectQualificationRequest'))
-    RTurk::RejectQualificationRequest(:qualification_request_id => "123456789", :reson => "Your work is terrible!") rescue RTurk::InvalidRequest
+    RTurk::RejectQualificationRequest(:qualification_request_id => "123456789", :reason => "Your work is terrible!") rescue RTurk::InvalidRequest
   end
   
   it "should parse and return the result" do
-    RTurk::RejectQualificationRequest(:qualification_request_id => "123456789", :reson => "Your work is terrible!").should be_a_kind_of RTurk::Response
+    RTurk::RejectQualificationRequest(:qualification_request_id => "123456789", :reason => "Your work is terrible!").should be_a_kind_of RTurk::Response
     RTurk::RejectQualificationRequest(:qualification_request_id => "789RVWYBAZW00EXAMPLE951RVWYBAZW00EXAMPLE").elements.should eql(
       {"RejectQualificationRequestResult"=>{"Request"=>{"IsValid"=>"True"}}}
     )
