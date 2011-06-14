af624e6d35020c63895d6fb5b01f5452b2ace3c6
Alex Chaffee <alex@stinky.com>
2011-06-13 19:58:57 -0700

    rename Question to ExternalQuestion
    Conflicts:
    	spec/operations/create_hit_spec.rb

--- lib/rturk/builders/external_question_builder.rb
+++ lib/rturk/builders/external_question_builder.rb
@@ -2,51 +2,51 @@ require 'cgi'
 require 'uri'
 
 module RTurk
-  class Question
-    
+  class ExternalQuestion
+
     attr_accessor :url, :url_params, :frame_height
-    
+
     def initialize(url, opts = {})
       @url = url
       self.frame_height = opts.delete(:frame_height) || 400
       self.url_params = opts
     end
-    
+
     def querystring
       @url_params.collect { |key, value| [CGI.escape(key.to_s), CGI.escape(value.to_s)].join('=') }.join('&')
     end
-    
+
     def url
       unless querystring.empty?
         # slam the params onto url, if url already has params, add 'em with a &
-        u = @url.index('?') ? "#{@url}&#{querystring}" : "#{@url}?#{querystring}" 
+        u = @url.index('?') ? "#{@url}&#{querystring}" : "#{@url}?#{querystring}"
       else
         u = @url
       end
       CGI.escapeHTML(u) #URL should be XML/HTML escaped
     end
-    
+
     def params
       @url_params
     end
-    
+
     def params=(param_set)
       @url_params = param_set
     end
-    
+
     def to_params
       raise RTurk::MissingParameters, "needs a url to build an external question" unless @url
       # TODO: update the xmlns schema... maybe
       xml = <<-XML
 <ExternalQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2006-07-14/ExternalQuestion.xsd">
-  <ExternalURL>#{url}</ExternalURL>	
+  <ExternalURL>#{url}</ExternalURL>
   <FrameHeight>#{frame_height}</FrameHeight>
 </ExternalQuestion>
       XML
       xml
     end
-    
+
   end
-  
-  
+
+
 end
--- spec/builders/external_question_spec.rb
+++ spec/builders/external_question_spec.rb
@@ -1,52 +1,52 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 require 'cgi'
 
-describe RTurk::Question do
-  
+describe RTurk::ExternalQuestion do
+
   before(:all) do
   end
-  
+
   it "should build a question" do
-    question = RTurk::Question.new('http://mpercival.com')
-    question.url.should == 'http://mpercival.com' 
+    question = RTurk::ExternalQuestion.new('http://mpercival.com')
+    question.url.should == 'http://mpercival.com'
     question.frame_height.should == 400
   end
 
   it "should build a question " do
-    question = RTurk::Question.new('http://mpercival.com', :frame_height => 500, :chapter => 1)
+    question = RTurk::ExternalQuestion.new('http://mpercival.com', :frame_height => 500, :chapter => 1)
     question.params[:chapter] = 2
-    question.url.should == 'http://mpercival.com?chapter=2' 
+    question.url.should == 'http://mpercival.com?chapter=2'
     question.frame_height.should == 500
   end
-  
+
   it "should build a question " do
-    question = RTurk::Question.new('http://mpercival.com?myparam=true&anotherparam=12', :frame_height => 500)
+    question = RTurk::ExternalQuestion.new('http://mpercival.com?myparam=true&anotherparam=12', :frame_height => 500)
     question.url.should == 'http://mpercival.com?myparam=true&amp;anotherparam=12'
     question.frame_height.should == 500
   end
-  
+
   it "should build a question with params" do
-    params = RTurk::Question.new('http://google.com/', :id => 'foo').to_params
-    CGI.unescape(params).should == 
+    params = RTurk::ExternalQuestion.new('http://google.com/', :id => 'foo').to_params
+    CGI.unescape(params).should ==
     <<-XML
 <ExternalQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2006-07-14/ExternalQuestion.xsd">
-  <ExternalURL>http://google.com/?id=foo</ExternalURL>	
+  <ExternalURL>http://google.com/?id=foo</ExternalURL>
   <FrameHeight>400</FrameHeight>
 </ExternalQuestion>
     XML
   end
 
   it "should build a question without params" do
-    params = RTurk::Question.new('http://google.com/').to_params
-    CGI.unescape(params).should == 
+    params = RTurk::ExternalQuestion.new('http://google.com/').to_params
+    CGI.unescape(params).should ==
     <<-XML
 <ExternalQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2006-07-14/ExternalQuestion.xsd">
-  <ExternalURL>http://google.com/</ExternalURL>	
+  <ExternalURL>http://google.com/</ExternalURL>
   <FrameHeight>400</FrameHeight>
 </ExternalQuestion>
     XML
   end
-  
-  
-  
+
+
+
 end
