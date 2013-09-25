9973c567eb8300a1241cd1ee922c6340e61feb47
Alejandro Mansilla <amansilla@gmail.com>
2013-09-25 20:00:25 -0300

    UpdateQualificationScore operation implemented.

---
+++ lib/rturk/operations/update_qualification_score.rb
@@ -0,0 +1,25 @@
+# Operation to update a qualification score
+
+module RTurk
+  class UpdateQualificationScore < Operation
+    attr_accessor :qualification_type_id, :subject_id, :integer_value
+                  require_params :qualification_type_id, :subject_id
+
+    def parse(xml)
+      RTurk::GetQualificationScoreResponse.new(xml)
+    end
+
+    def to_params
+      params = {
+        'QualificationTypeId' => qualification_type_id
+        'SubjectId' => subject_idid
+      }
+      params['IntegerValue'] = integer_value unless integer_value.nil?
+      params
+    end
+  end
+
+  def self.UpdateQualificationScore(*args)
+    RTurk::UpdateQualificationScore.create(*args)
+  end
+end
