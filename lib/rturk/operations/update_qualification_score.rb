31dbe81424acfcf42c2bf2e5fc49e23517fbbb8c
Alex Egg <eggie5@gmail.com>
2013-10-10 22:27:57 -0700

    added UpdateQualificationScore api

---
+++ lib/rturk/operations/update_qualification_score.rb
@@ -0,0 +1,20 @@
+# Operation to update a qualification score
+
+module RTurk
+  class UpdateQualificationScore < Operation
+    attr_accessor :qualification_type_id, :qualification_type_score, :subject_id, :integer_value
+    require_params :qualification_type_id, :subject_id, :integer_value
+
+    def to_params
+      params = {
+        'QualificationTypeId' => qualification_type_id,
+        'SubjectId' => subject_id,
+        'IntegerValue'=> integer_value
+      }
+    end
+  end
+
+  def self.UpdateQualificationScore(*args)
+    RTurk::UpdateQualificationScore.create(*args)
+  end
+end
---
+++ spec/operations/update_qualification_score_spec.rb
@@ -0,0 +1,28 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
+
+describe RTurk::UpdateQualificationScore do
+  before(:all) do
+    aws = YAML.load(File.open(File.join(SPEC_ROOT, 'mturk.yml')))
+    RTurk.setup(aws['AWSAccessKeyId'], aws['AWSAccessKey'], :sandbox => true)
+    faker('update_qualification_score', :operation => 'UpdateQualificationScore')
+  end
+
+  it "should ensure required params" do
+    lambda{RTurk::UpdateQualificationScore()}.should raise_error RTurk::MissingParameters
+  end
+
+  it "should successfully request the operation" do
+    RTurk::Requester.should_receive(:request).once.with(
+      hash_including('Operation' => 'UpdateQualificationScore'))
+    RTurk::UpdateQualificationScore(:qualification_type_id => '789RVWYBAZW00EXAMPLE', 
+                                  :subject_id => "ABCDEF1234",
+                                  :integer_value => 80) rescue RTurk::InvalidRequest
+  end
+
+  it "should parse and return the result" do
+    response = RTurk::UpdateQualificationScore(:qualification_type_id => '789RVWYBAZW00EXAMPLE', 
+                                  :subject_id => "ABCDEF1234",
+                                  :integer_value => 80).elements.should eql(
+      {"UpdateQualificationScoreResult"=>{"Request"=>{"IsValid"=>"True"}}})
+  end
+end
